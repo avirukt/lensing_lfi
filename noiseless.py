62f8c495b4854bfc3b22a6aeab4b197a41c5a0df
Avirukt Mittal <avirukt@gmail.com>
2019-05-21 18:10:00 -0700

    Rename noiseless model

--- noiseless.py
+++ noiseless.py
@@ -26,5 +26,5 @@ def training_input_fn(shuffle_buffer=1000, batch_size=batch_size):
     return dataset.skip(nsims_test).map(parse).repeat().shuffle(shuffle_buffer).batch(batch_size)
 
 
-model = LFI(["field"], [r"$M_\nu$",r"$\Omega_m$",r"$\sigma_8$"], model_dir=add_commit_hash('/global/scratch/avirukt/models/convnonoise'))
+model = LFI(["field"], [r"$M_\nu$",r"$\Omega_m$",r"$\sigma_8$"], model_dir=add_commit_hash('/global/scratch/avirukt/models/noiseless_conv'))
 model.train(training_input_fn, max_steps=3*10**6//batch_size)
