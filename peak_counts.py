5274e9eaedbf5b6ae67b9cf1717426c9c3119fb3
Avirukt Mittal <avirukt@gmail.com>
2019-06-07 13:27:18 -0700

    Use smoothly decaying lr, reorganise a bit

--- peak_counts.py
+++ peak_counts.py
@@ -1,28 +1,13 @@
 from power_spectrum import *
 import sys
 
-p = np.load("params_conc.npy")
-x = np.load("data_scaled.npy")
-tot_nsims = len(p)
+model = LFI(["field"], 
+            [r"$M_\nu$",r"$\Omega_m$",r"$A_s\times10^9$"], 
+            model_dir=sys.argv[1], 
+            learning_rate=lambda x: tf.train.exponential_decay(0.001, x, 30000, 0.1, staircase=False)
+           )
 
-labels = [r"$M_\nu$",r"$\Omega_m$",r"$A_s\times10^9$"]
-nsims_train = 10**4
-nsims_test = tot_nsims - nsims_train
-
-indices = np.arange(tot_nsims)
-random.shuffle(indices)
-x_train = array([x[ind] for ind in indices[:nsims_train]])
-p_train = array([p[ind] for ind in indices[:nsims_train]])
-x_test = array([x[ind] for ind in indices[nsims_train:]])
-p_test = array([p[ind] for ind in indices[nsims_train:]])
-
-save("data_peak_counts/x_train.npy", x_train)
-save("data_peak_counts/x_test.npy", x_test)
-save("data_peak_counts/p_train.npy", p_train)
-save("data_peak_counts/p_test.npy", p_test)
-
-batch_size = 128
-
-model = LFI(["field"], labels, model_dir=sys.argv[1], learning_rate=lambda x: tf.train.exponential_decay(0.001, x, 30000, 0.1, staircase=True))
-
-model.train(training_fn_generator(x_train,p_train, batch_size=batch_size), max_steps=100000)
+model.train(
+    training_fn_generator(load("data_peak_counts/x_train.npy"), load("data_peak_counts/p_train.npy"), batch_size=128), 
+    max_steps=100000
+)
