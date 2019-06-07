7cfc5593ef38f7fd2337eaf52e149130832922f5
Avirukt Mittal <avirukt@gmail.com>
2019-06-07 13:20:06 -0700

    Use step decaying lr, save shuffled test/train split

--- peak_counts.py
+++ peak_counts.py
@@ -16,8 +16,13 @@ p_train = array([p[ind] for ind in indices[:nsims_train]])
 x_test = array([x[ind] for ind in indices[nsims_train:]])
 p_test = array([p[ind] for ind in indices[nsims_train:]])
 
+save("data_peak_counts/x_train.npy", x_train)
+save("data_peak_counts/x_test.npy", x_test)
+save("data_peak_counts/p_train.npy", p_train)
+save("data_peak_counts/p_test.npy", p_test)
+
 batch_size = 128
 
-model = LFI(["field"], labels, model_dir=sys.argv[1], learning_rate=lambda x: tf.cond(x<50000, lambda: 0.001, lambda: 0.0001))
+model = LFI(["field"], labels, model_dir=sys.argv[1], learning_rate=lambda x: tf.train.exponential_decay(0.001, x, 30000, 0.1, staircase=True))
 
 model.train(training_fn_generator(x_train,p_train, batch_size=batch_size), max_steps=100000)
