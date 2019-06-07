32a9c647f0fa25ff1768d7c229b78e2bb569e260
Avirukt Mittal <avirukt@gmail.com>
2019-06-07 14:41:25 -0700

    Change peak count lr

--- peak_counts.py
+++ peak_counts.py
@@ -4,7 +4,7 @@ import sys
 model = LFI(["field"], 
             [r"$M_\nu$",r"$\Omega_m$",r"$A_s\times10^9$"], 
             model_dir=sys.argv[1], 
-            learning_rate=lambda x: tf.train.exponential_decay(0.001, x, 20000, 0.3, staircase=True)
+            learning_rate=lambda x: tf.train.exponential_decay(0.001, x, 20000, 0.3, staircase=False)
            )
 
 model.train(
